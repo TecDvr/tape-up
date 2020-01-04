9335b4b3dc849526513f2d904f0740a45ae22527
Zach Gwirtz <zachgw@gmail.com>
2020-01-04 14:23:22 -0800

    init animation test

---
+++ app.js
@@ -0,0 +1,29 @@
+const tapingUp = document.querySelector('.tapeUp')
+const video = tapingUp.querySelector('video')
+
+
+
+const controller = new ScrollMagic.Controller();
+
+let scene = new ScrollMagic.Scene({
+    duration: 9000,
+    triggerElement: tapingUp,
+    triggerHook: 0
+})
+    .addIndicators()
+    .setPin(tapingUp)
+    .addTo(controller);
+
+    let accelamount = 0.1;
+    let scrollpos = 0;
+    let delay = 0;
+    
+    scene.on("update", e => {
+        scrollpos = e.scrollPos / 1000;
+    });
+    
+    setInterval(() => {
+        delay += (scrollpos - delay) * accelamount;
+      
+        video.currentTime = delay;
+    }, 33.3);
